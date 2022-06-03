3dcc3da598e8ca85daa609a333477050060e6dd0
MayYu <ftss0216@gmail.com>
2022-06-03 11:19:27 +0800

    1st Version

---
+++ Git/src/GitTset/MainTest.java
@@ -0,0 +1,10 @@
+package GitTset;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("第一次上傳");
+	}
+
+}
---
+++ Git/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mayyu
+ *
+ */
+module Git {
+}
