5a806328b222862dc5391fb4a45365d25b829cd8
MayYu <ftss0216@gmail.com>
2022-06-03 11:37:29 +0800

    Revert "2 Version"
    This reverts commit 426de73ed5f2cd00ebbe64c91a8b50f6bbd1983c.

--- Git/src/GitTset/MainTest.java
+++ Git/src/GitTset/MainTest.java
@@ -5,11 +5,6 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("第一次上傳");
-		Math.add(10, 30);
 	}
-}
-class Math{
-	static int add(int x, int y) {
-		return x+y;
-	}
+
 }
