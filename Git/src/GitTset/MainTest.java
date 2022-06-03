426de73ed5f2cd00ebbe64c91a8b50f6bbd1983c
MayYu <ftss0216@gmail.com>
2022-06-03 11:25:25 +0800

    2 Version

--- Git/src/GitTset/MainTest.java
+++ Git/src/GitTset/MainTest.java
@@ -5,6 +5,11 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("第一次上傳");
+		Math.add(10, 30);
 	}
-
+}
+class Math{
+	static int add(int x, int y) {
+		return x+y;
+	}
 }
